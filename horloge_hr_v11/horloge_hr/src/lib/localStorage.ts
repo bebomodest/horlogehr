4076a3d6902c759a09c8e692d0dfb65691b7e216
bebomodest <bebomodest@gmail.com>
2026-08-14 22:35:17 +0300

    Delete horloge_hr_v11/horloge_hr directory

--- horloge_hr_v11/horloge_hr/src/lib/localStorage.ts
+++
@@ -1,8 +0,0 @@
-export const saveToLocal = (key: string, data: any) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const getFromLocal = (key: string) => {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : [];
-};
